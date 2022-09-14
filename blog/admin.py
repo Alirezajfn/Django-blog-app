3f04536a5b26d5609ce0e5584a7be1447c0409ce
Alireza <arwix79@gmail.com>
2022-09-15 01:52:55 +0430

    Update django admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import register
+
 from blog.models import *
 
 
+class PostCategoryInline(admin.TabularInline):
+    model = PostCategory
+    extra = 1
+
+
+@register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'author', 'published', 'created_at', 'updated_at', 'published_at')
     list_filter = ('published', 'created_at', 'updated_at', 'published_at')
@@ -9,8 +17,18 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'created_at'
     ordering = ('-created_at',)
+    inlines = [PostCategoryInline]
 
 
-admin.site.register(Category)
+@register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ('post', 'category')
+    list_filter = ('post', 'category')
+    search_fields = ('post', 'category')
+
+
+admin.site.register(Category, inline=PostCategoryInline)
 admin.site.register(Tag)
-admin.site.register(Post, PostAdmin)
+admin.site.register(PostTag)
+admin.site.register(Comment)
+admin.site.register(PostMeta)
