d426634cc5f9fbd2e0cf72aa7c17dffb9cbc7092
Alireza <arwix79@gmail.com>
2022-09-15 23:21:35 +0430

    Using class based views in views.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from blog import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('posts/', views.posts_list, name='posts_list'),
+    path('posts/', views.PostListView.as_view(), name='posts_list'),
     path('details/<slug:slug>', views.post_detail, name='post_detail'),
-    path('category/<slug:slug>', views.category_detail, name='category_detail'),
-    path('tag/<slug:slug>', views.tag_detail, name='tag_detail'),
+    path('category/<slug:slug>', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('tag/<slug:slug>', views.TagDetailView.as_view(), name='tag_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+
 from blog.models import *
 
 
@@ -7,10 +9,19 @@ def home(request):
     return HttpResponse('Hello, World!')
 
 
-def posts_list(request):
-    posts = Post.objects.all()
-    context = "\n".join([f"{post.title}, {post.content}" for post in posts])
-    return HttpResponse(context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(published=True)
+
+
+# def posts_list(request):
+#     posts = Post.objects.all()
+#     context = "\n".join([f"{post.title}, {post.content}" for post in posts])
+#     return HttpResponse(context)
 
 
 def post_detail(request, slug):
@@ -21,17 +32,25 @@ def post_detail(request, slug):
     return HttpResponse(f"Post detail for {post.title}, {post.content}")
 
 
-def category_detail(request, slug):
-    posts = Post.objects.filter(published=True).filter(categories__category__slug=slug)
-    if not posts:
-        return HttpResponse('Category not found')
-    context = "\n".join([f"{post.title}, {post.content}" for post in posts])
-    return HttpResponse(context)
+class CategoryDetailView(PostListView):
+    def get_queryset(self):
+        return Post.objects.filter(published=True).filter(categories__category__slug=self.kwargs['slug'])
 
 
-def tag_detail(request, slug):
-    posts = Post.objects.filter(published=True).filter(tags__tag__slug=slug)
-    if not posts:
-        return HttpResponse('Tag not found')
-    context = "\n".join([f"{post.title}, {post.content}" for post in posts])
-    return HttpResponse(context)
+# def category_detail(request, slug):
+#     posts = Post.objects.filter(published=True).filter(categories__category__slug=slug)
+#     if not posts:
+#         return HttpResponse('Category not found')
+#     context = "\n".join([f"{post.title}, {post.content}" for post in posts])
+#     return HttpResponse(context)
+
+class TagDetailView(PostListView):
+    def get_queryset(self):
+        return Post.objects.filter(published=True).filter(tags__tag__slug=self.kwargs['slug'])
+
+# def tag_detail(request, slug):
+#     posts = Post.objects.filter(published=True).filter(tags__tag__slug=slug)
+#     if not posts:
+#         return HttpResponse('Tag not found')
+#     context = "\n".join([f"{post.title}, {post.content}" for post in posts])
+#     return HttpResponse(context)
