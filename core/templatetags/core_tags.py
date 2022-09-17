5ae5dcc8eb646182a058c6f69b3e2314ec3dcd83
Alireza <arwix79@gmail.com>
2022-09-18 00:49:31 +0430

    Added template tags for most sections of index.html

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,18 +1,17 @@
 from django import template
 
-from blog.models import Post, PostImage, Category
-from mysite.settings import MEDIA_URL
+from blog.models import Post, Category
+
 
 register = template.Library()
+posts = Post.objects.filter(published=True)
 
 
 @register.inclusion_tag('core/hero-slider.html')
 def popular_posts():
-    posts = Post.objects.filter(published=True).order_by('-published_at')[:4]
-    for post in posts:
-        post.images.set(PostImage.objects.filter(post=post).all())
-        print(post.images.first().image.url)
-    return {'posts': posts}
+    p = posts.order_by('-published_at')[:4]
+    #p = posts.order_by('-views')[:4]
+    return {'posts': p}
 
 
 @register.inclusion_tag('core/core-categories.html')
@@ -20,4 +19,22 @@ def posts_categories():
     categories = Category.objects.filter(top_level=True)[:8]
     if len(categories) < 8:
         categories |= Category.objects.filter(top_level=False)[:8 - len(categories)]
+    return {'categories': categories}
+
+
+@register.inclusion_tag('core/core-footer-recent-posts.html')
+def recent_posts():
+    p = posts.order_by('-published_at')[:4]
+    return {'posts': p}
+
+
+@register.inclusion_tag('core/core-trending-section.html')
+def trending_posts():
+    p = posts.order_by('-views')[:5]
+    return {'posts': p}
+
+
+@register.inclusion_tag('core/core-header-categories.html')
+def header_categories():
+    categories = Category.objects.filter(parent=None)
     return {'categories': categories}
